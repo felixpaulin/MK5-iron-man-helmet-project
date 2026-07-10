f37bf0b8451e1fa3c7ab5387b1f356631f6291d5
FelixP <felix12.paulin@gmail.com>
2026-07-10 16:24:47 +0200

    Implement SerialPort class methods for opening, closing, and sending data over USB Serial

--- pc/SerialPort.cpp
+++ pc/SerialPort.cpp
@@ -38,3 +38,87 @@ SerialPort::SerialPort(const std::string& portName)
     this->portName = portName;
     this->handle = nullptr;
 }
+
+SerialPort::~SerialPort()
+{
+    close();
+}
+
+bool SerialPort::open()
+{
+    handle = CreateFileA(
+        portName.c_str(),
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        nullptr,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        nullptr
+    );
+
+    if (handle == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Failed to open " << portName << std::endl;
+        handle = nullptr;
+        return false;
+    }
+
+    DCB serialParams = {};
+    serialParams.DCBlength = sizeof(serialParams);
+
+    if (!GetCommState(handle, &serialParams))
+    {
+        std::cout << "Failed to read COM settings." << std::endl;
+        close();
+        return false;
+    }
+
+    serialParams.BaudRate = CBR_115200;
+    serialParams.ByteSize = 8;
+    serialParams.StopBits = ONESTOPBIT;
+    serialParams.Parity = NOPARITY;
+
+    if (!SetCommState(handle, &serialParams))
+    {
+        std::cout << "Failed to configure COM port." << std::endl;
+        close();
+        return false;
+    }
+
+    std::cout << "Connected to " << portName << std::endl;
+
+    return true;
+}
+
+void SerialPort::close()
+{
+    if (handle != nullptr)
+    {
+        CloseHandle(handle);
+        handle = nullptr;
+    }
+}
+
+bool SerialPort::send(const std::string& message)
+{
+    if (!isOpen())
+        return false;
+
+    DWORD bytesWritten;
+
+    BOOL success = WriteFile(
+        handle,
+        message.c_str(),
+        (DWORD)message.length(),
+        &bytesWritten,
+        nullptr
+);
+
+return success && bytesWritten == message.length();
+}
+
+bool SerialPort::isOpen() const
+{
+    return handle != nullptr;
+}
+
