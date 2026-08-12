1a68a4472031757f6ea2f7a32cac710d1667bf20
FelixP <133700078+felixpaulin@users.noreply.github.com>
2026-08-12 14:05:21 +0200

    Update SerialPort initialization to use COM3 for correct connection

--- pc/TinyMLListener.cpp
+++ pc/TinyMLListener.cpp
@@ -37,7 +37,7 @@ int main()
     std::cout << " MK5 TinyML Desktop Listener\n";
     std::cout << "=========================================\n\n";
 
-    SerialPort helmet("COM6");
+    SerialPort helmet("COM3");
 
 
     if (!helmet.open())
