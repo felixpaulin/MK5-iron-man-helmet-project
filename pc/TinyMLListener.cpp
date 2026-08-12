f00877a21ea22c6a98a9ecc0a0ced483d824ff07
FelixP <felix12.paulin@gmail.com>
2026-08-12 13:39:42 +0200

    Update SerialPort COM number from COM3 to COM7 for correct connection

--- pc/TinyMLListener.cpp
+++ pc/TinyMLListener.cpp
@@ -37,7 +37,7 @@ int main()
     std::cout << " MK5 TinyML Desktop Listener\n";
     std::cout << "=========================================\n\n";
 
-    SerialPort helmet("COM3");
+    SerialPort helmet("COM7");
 
 
     if (!helmet.open())
